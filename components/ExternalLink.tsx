ad6b81fdae76bb6b556643ab8cb16203713b49eb
htmcharles <hatumacharles1@gmail.com>
2024-12-25 20:17:30 +0200

    uber

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -4,7 +4,6 @@ import { type ComponentProps } from 'react';
 import { Platform } from 'react-native';
 
 type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: string };
-
 export function ExternalLink({ href, ...rest }: Props) {
   return (
     <Link
